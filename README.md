c682907785b9e06dc17f7acbef4305ed312eb5f1
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-05-31 13:52:16 -0700

    wip

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# spotify-top-finder
-Use Spotify API to find the user's top artists, songs, albums.
+# Top Spotify Stats and Visualization
+The command-line application showcases the user's top 10 songs on Spotify and visualizes data such as Beats Per Minute (BPM) and valence through interactive graphs. It also lists the user's top artists. The process begins by requesting user authorization to access their Spotify data, after which the results are displayed.
+
+Tech used: Python, Spotify API, Spotipy Library, Plotext, Pandas
+
+### TODO: explain how to run it locally
+
+## Purpose:
+I wanted to get more familar with using Spotify's API and the Spotipy Library. I also wanted to see how I can visualize a track's audio features
+
+## What I learned:
+I learned about plotext for the first time and I found it very nice to have graphs be displayed on the command line. I was also not very familar about
+different graphs so I was able to test multiple to see which best represents the data I have.
+
+made with 💚 by sama ahmed
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from spotipy.oauth2 import SpotifyOAuth
 import os
 from dotenv import load_dotenv
 import plotext
+import pandas as pd
 
 load_dotenv()
 
@@ -11,44 +12,75 @@ SPOTIFY_CLIENT_ID=os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET=os.getenv("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI=os.getenv("SPOTIFY_REDIRECT_URI")
 
-
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri=SPOTIFY_REDIRECT_URI, scope=scope))
 
 print("\n*  ੈ✩‧₊˚*  ੈ✩‧₊˚*  ੈ✩ Welcome to YourSpotStats ‧₊˚*  ੈ✩‧₊˚*  ੈ✩‧₊˚*\n")
 
 # Top Tracks this month
 range = "short_term"
-tracks = spotify.current_user_top_tracks(time_range=range, limit=10)
 tempos = []
 trackNames = []
 valences = []
-#print("+--------------------------------------------------------------------+")
-for i, track in enumerate(tracks['items']):
+artistName = []
+duration = []
 
-  # Convert from ms to min and secs
-  m_sec = track['duration_ms']
-  total_sec = m_sec / 1000
-  min = int(total_sec // 60)
-  sec = int(total_sec % 60)
-  
-  #TODO: If i have a number like 4, make it 04 so it looks like 3:04 instead of 3:4
+def getTopTracks():
+  topTracks = spotify.current_user_top_tracks(time_range=range, limit=10)
 
-  print('|', track['name'], "by", track['artists'][0]['name'])
-  #print('|', 'Duration', min, ':', sec)
-  #print('|', 'Tempo: ', spotify.audio_features(track['uri'])[0]['tempo'])
-  trackNames.append(track['name'])
-  tempos.append(spotify.audio_features(track['uri'])[0]['tempo'])
-  #print('|', 'Valence: ', spotify.audio_features(track['uri'])[0]['valence'])
-  valences.append(spotify.audio_features(track['uri'])[0]['valence'])
-  #print('|', "----------------------------------------------------------------------\n")
+  for track in topTracks['items']:
 
+    # Convert from ms to min and secs
+    m_sec = track['duration_ms']
+    total_sec = m_sec / 1000
+    min = int(total_sec // 60)
+    sec = int(total_sec % 60)
+    
+    #TODO: If i have a number like 4, make it 04 so it looks like 3:04 instead of 3:4
+    
+    artistName.append(track['artists'][0]['name'])
+    duration.append(str())
+    #print('|', 'Duration', min, ':', sec)
+    #TODO:store the duation of each song to be displayed
+    
+    trackNames.append(track['name'])
+    tempos.append(spotify.audio_features(track['uri'])[0]['tempo'])
+    valences.append(spotify.audio_features(track['uri'])[0]['valence'])
+
+  # Create Data Frame
+  data = pd.DataFrame( {
+    "Track": trackNames,
+    "Artist": artistName,
+    "Tempo": tempos,
+    "Valences": valences
+  })
+
+  #data.style.set_properties(**{'border': '1.3px solid white', 'color': 'blue'})
+  #data.style.set_properties(subset=['trackNames'], **{'width': '100px'})
+  print(",.-~*´¨¯¨`*·~-.¸- Your Top 10 Songs This Month -,.-~*´¨¯¨`*·~-.¸\n\n")
+  print(data)
+
+# User's Top 10 Songs
+def getTopArtists():
+  artistList = []
+  topArtists = spotify.current_user_top_artists(time_range=range,limit=10)
+  for artist in topArtists['items']:
+    artistList.append(artist['name'])
+
+  data2 = pd.DataFrame( {
+    "Artist": artistList
+  })
+
+  print(",.-~*´¨¯¨`*·~-.¸- Your Top 10 Artists This Month -,.-~*´¨¯¨`*·~-.¸\n\n")
+  print(data2)
+
+'''
 print('\n\n')
 plotext.bar(trackNames, tempos, orientation="horizontal")
 plotext.title('Tempo Visualization (Bar Chart)')
 plotext.show()
 
-
 print('\n\n')
 plotext.hist(valences)
 plotext.title('Valence Visualization (Histogram)')
 plotext.show()
+'''
